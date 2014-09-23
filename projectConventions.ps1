56020ceb571face320894b4919b46be420c73e60
Shaun <shaun.finglas@googlemail.com>
2014-09-23 17:21:43 +0100

    Formatting complete.

--- projectConventions.ps1
+++ projectConventions.ps1
@@ -3,8 +3,7 @@
 
     if ($projectName.Contains("Test")) {
         if (!$projectName.EndsWith("UnitTests") -and !$projectName.EndsWith("IntegrationTests")) {
-            $message = $projectName + " is not named correctly. Must end with UnitTests or IntegrationTests."
-            echo $message
+            echo ($projectName + " is not named correctly. Must end with UnitTests or IntegrationTests.")
         }
     } 
 }
@@ -14,9 +13,8 @@ function CheckRootNamespace($project) {
     $rootNamespace = cat $project.FullName | Select-string "<RootNamespace>"
     $expectedRootNamespace = ("<RootNamespace>" + $projectName + "</RootNamespace>")
 
-    if (!$rootNamespace.Line.Trim().Equals($expectedRootNamespace.Trim())) {
-        $message = $projectName + " root namespaces is not named correctly. Should match project name."
-        echo $message
+    if (!$rootNamespace.Line.Trim().Equals($expectedRootNamespace)) {
+        echo ($projectName + " root namespaces is not named correctly. Should match project name.")
     }
 
 }
@@ -26,9 +24,26 @@ function CheckAssemblyName($project) {
     $assemblyName = cat $project.FullName | Select-string "<AssemblyName>"
     $expectedAssemblyName = ("<AssemblyName>" + $projectName + "</AssemblyName>")
 
-    if (!$assemblyName.Line.Trim().Equals($expectedAssemblyName.Trim())) {
-        $message = $projectName + " assembly name is not named correctly. Should match project name."
-        echo $message
+    if (!$assemblyName.Line.Trim().Equals($expectedAssemblyName)) {
+        echo ($projectName + " assembly name is not named correctly. Should match project name.")
+    }
+}
+
+function CheckWarningsAsErrors($project) {
+    $projectName = ProjectName $project
+    $assemblyName = cat $project.FullName | Select-string "<TreatWarningsAsErrors>"
+    $expectedAssemblyName = ("<TreatWarningsAsErrors>true</TreatWarningsAsErrors>")
+
+    if (!$assemblyName.Line) {
+        echo ($projectName + " needs to have warnings as errors enabled.")
+    }
+
+    foreach ($line in $assemblyName.Line) {
+
+        if (!$line.Trim().Equals($expectedAssemblyName)) {
+            echo ($projectName + " should have warnings as errors enabled for Debug and Release builds.")
+            return
+        } 
     }
 }
 
@@ -37,12 +52,14 @@ function ProjectName($project) {
 }
 
 function CheckProjectFiles() {
-    $projects = Get-ChildItem .\ -Recurse -Filter "*.csproj"
+    $projects = Get-ChildItem .\ -Recurse -Filter "*.csproj" | Where-Object { $_.Name -match "Asos.*"}
 
     foreach ($project in $projects) {
+        echo $project.Name
         CheckProjectName $project
         CheckAssemblyName $project
         CheckRootNamespace $project
+        CheckWarningsAsErrors $project
     }
 }
 
